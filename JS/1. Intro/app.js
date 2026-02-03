c332be93015f2c3c65141d3d43d59bda58930fee
Anant <acera52024@gmail.com>
2026-02-03 13:02:29 +0530

    js

---
+++ JS/1. Intro/app.js
@@ -0,0 +1,51 @@
+// console.log("hello world")
+
+
+// declare -> 
+// let a = 10
+// let b = 20
+// var c = a + b
+// console.log(c)
+
+// const -> 
+
+// let a = 10
+// a = 30
+
+// const b = 30
+
+// const c = 40
+
+// b = c
+
+// const pie = 3.14
+
+
+// a = -10.5
+// console.log(typeof(a))
+
+// a = "10"
+// b = 20
+// console.log(a+b)
+// console.log(typeof(a))
+// console.log(typeof(b))
+
+
+// let a = 10
+// let b = 20
+// let c = a + b
+// console.log(c)
+// // Your sum of a and b is 30
+// console.log("Your sum of a and b is", c)
+// // Your sum of 10 and 20 is 30
+
+// // template literal -> display optimal
+// console.log(`Your sum of ${a} and ${b} is ${a+b}`)
+// console.log("Your sum of", a, "and", b, "is", c)
+
+// alert -> 
+// alert("Something went wrong")
+
+console.log("Normal display")
+console.warn("Warning")
+console.error("error")
