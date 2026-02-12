0e9e393e0b7718af304ee89260be9c93054ebf0c
Anant <acera52024@gmail.com>
2026-02-12 12:51:08 +0530

    js

--- JS/4. Function/app.js
+++ JS/4. Function/app.js
@@ -1,40 +1,157 @@
 // function creation
-function hello() {
-    console.log("hello JS")
-}
+// function hello() {
+//     console.log("hello JS")
+// }
 
 
 // arguments 
-function hello_count(n) {
+// function hello_count(n) {
 
-    for (let i = 1; i <= n; i++) {
-        console.log("Hello JS")
-    }
-}
+//     for (let i = 1; i <= n; i++) {
+//         console.log("Hello JS")
+//     }
+// }
 
 
-function evenCheck(n){
-    if(n % 2 == 0){
-        console.log("Even")
-    }
-    else{
-        console.log("Odd")
-    }
-}
+// function evenCheck(n){
+//     if(n % 2 == 0){
+//         console.log("Even")
+//     }
+//     else{
+//         console.log("Odd")
+//     }
+// }
 // function calling
 // parameters -> value provide during function calling
 // hello_count(50)
 // function help in code reusability -> 
 
-evenCheck(23)
-evenCheck(65)
-evenCheck(20)
-evenCheck(1)
-evenCheck(8)
-evenCheck(76)
-evenCheck(99)
+// evenCheck(23)
+// evenCheck(65)
+// evenCheck(20)
+// evenCheck(1)
+// evenCheck(8)
+// evenCheck(76)
+// evenCheck(99)
 
 // function - type - normal, return (int , string , boolean)
 // block scope , function scope , lexical scope
 
+// functuion creation
+// function sum(a, b) {
+//     console.log(a + b)
+// }
 
+// normal => para -> args -> logic -> display -> end to end 
+// int , string , boolean
+
+// return - int 
+function sum_int(a, b) {
+    return a + b
+}
+
+// return - string
+function check(n) {
+    if (n >= 18) {
+        return "True"
+    }
+    else {
+        return "False"
+    }
+}
+
+// return - boolean
+function check_bool(n) {
+    if (n >= 18) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function Pallindrome(n) {
+    let temp = n // 121 12 1 0
+    let sum = 0 // 0 1 12 121
+    while (temp != 0) { // T T T
+
+        let rem = temp % 10 // rem = 1%10 = 1
+        sum = sum * 10 + rem // sum = 12 * 10 + 1 = 121
+        temp = parseInt(temp / 10) // 121/10 - 12/10 -> 1/10 -> 0
+    }
+
+    if (sum == n) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+
+function leapYear(n) {
+    // 4 % ==0 && 100 != 0 || 400 % ==0 
+    if (n % 4 == 0 && (n % 100 != 0 || n % 400 == 0)) {
+        return "Leap Year"
+    }
+    else {
+        return "Not a Leap Year"
+    }
+}
+
+// function calling
+// console.log(Pallindrome(123))
+// console.log(Pallindrome(121))
+// console.log(Pallindrome(154))
+// console.log(Pallindrome(65432))
+// console.log(Pallindrome(1234))
+
+// console.log(leapYear(2004))
+// console.log(leapYear(1999))
+// console.log(leapYear(2021))
+// console.log(leapYear(2025))
+// console.log(leapYear(2029))
+// console.log(leapYear(2088))
+
+
+// Variable ko Access krne ka Tarika ya range
+// Block scope 
+// Function Scope
+    // jo var apne func me andr define kra hai toh woh var func ke andr hi use hoga
+// Lexical Scope
+
+
+// // global variable
+// let a = 100
+// {
+//     // local variable
+//     let a = 10
+
+// }
+
+// console.log(a)
+
+
+// function cal(a,b){
+//     let sum = a+b
+//     return sum
+// }
+
+
+// console.log(cal(10,20))
+
+
+// lexical scope
+// function -> function call hoga tbhi kaam hoga
+// function outerfunc(){
+//     let a = 10
+//     let b = 20
+
+//     function innerfunc(){
+//         console.log(a)
+//         console.log(b)
+//     }
+//     innerfunc()
+// }
+
+// outerfunc()
---
+++ JS/5. ObjectLiteral/app.js
@@ -0,0 +1,184 @@
+// let info = ["Aman" , 98 , 24]
+
+// complex understanding ko easy krne k liye object literal -> 
+// apke data ko key : value - pairs -> JSON -> JavaScript Object Notation
+
+// Python - dictionary 
+//  MongoDB -
+
+// key : value ->Property
+// Collection of properties - object 
+
+// let info = {
+//     name : "Aman",
+//     age : 24,
+//     marks : 98
+// }
+
+// mutable -> value ko reassign -> key ke threw kr sakthe ho 
+
+// console.log(info)
+// // info.marks = 100
+// info["marks"] = 100
+
+// console.log(info)
+
+// // key present nhi hai
+
+// info.gender = "Male"
+// console.log(info)
+
+
+// object of objects - nested of object  - 
+// let info = {
+//     name : "Aman",
+//     age : 24,
+//     marks : {
+//         maths : 98,
+//         hindi : 56,
+//         comp  : 67
+//     }
+// }
+
+// console.log(info)
+// info.marks.hindi = 65
+// // info["marks"]["hindi"] = 65
+// console.log(info)
+
+// info.marks.chem = 78
+// console.log(info)
+
+
+// const classinfo = {
+// 	Aman : {
+// 		Age : 28,
+// 		City : "Delhi"
+// 		},
+
+// 	Mukesh : {
+// 		Age : 23 ,
+// 		City : "Gurugram"
+// 		},
+
+// 	Sahil : {
+// 		Age : 26,
+// 		City : "Gurugram"
+// 		}
+// 		};
+
+// console.log(classinfo)
+// classinfo.Sahil.City = "Delhi"
+// console.log(classinfo)
+
+// Array of Object
+// const classinfo2 = [
+// {
+// 	Name : "Mukesh",
+// 	Age : 23 ,
+// 	City : "Gurugram"
+// },
+// {
+// 	Name : "Sahil",
+// 	Age : 23 ,
+// 	City : "Gurugram"
+
+// },
+// {
+// 	Name : "Aman",
+// 	Age : 23 ,
+// 	City : "Gurugram"
+// },
+// {
+// 	Name : "Karan",
+// 	Age : 23 ,
+// 	City : "Gurugram"
+// }
+// ]
+
+
+// 3.14 - 22/7
+// console.log(Math.PI)
+// console.log(Math.E)
+// console.log(Math.random())
+// // absolute only positive - decimal/decimal
+// console.log(Math.abs(-10.5))
+//  0 - 1 -> 1 not included 
+// let a = 5
+// console.log(a * a  * a)
+// console.log(a ** 3)
+// console.log(Math.pow(a,3))
+
+
+// Exception Handling -> 
+
+// Exception -> ek unexcepted error jo apke normal flow ko khrb krd
+
+// try -> risky code 
+// catch, -> catch ke passs akr apka catch us galti ko handle krlega 
+//  finally -> try m galti ho ya na ho finally hamesha 
+
+
+
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+
+// try {
+//     console.log("a") // risky code
+// }
+// catch {
+//     console.log("Exception Handled")
+// }
+// finally{
+//     console.log("Always Executed")
+// }
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+
+// customized galti ko likhna ho -> throw raise an exception
+// 
+// let n =9
+// if(n<=18){
+//     throw new "age nhi hai 18 se under hai"
+// }
+// else{
+//     console.log("Allowed")
+// }
+
+// recursion -> function ke andr dubara ussi function ko call krlethe ho 
+// recursion -> time/space < loop
+// loop - si , ei , update 
+
+// base case / kaam
+
+function sum_of_natural(n){ // 5
+    let sum = 0;
+    for(let i = 1 ; i<= n ; i++){
+        sum = sum + i
+    }
+    return sum
+}
+
+// var + loop n + hr iteration pr sum ki value ko lekr aa rhe or + plus bhi kr rhe ho
+
+
+function sum_rec(n){
+    //base case 
+    if(n== 0){
+        return n
+        // jo kaam bola woh krdo
+        // isse phele jitna bhi call stack fill kra hai utna pura apko khali krna hoga
+    }
+    // kaam
+    return n + sum_rec(n-1)
+
+}
+
+// fibo + asc + DOM 
