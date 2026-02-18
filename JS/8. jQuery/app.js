27ee8f301c9541ed62864be30eec915afc9bdcd7
Anant <acera52024@gmail.com>
2026-02-18 12:32:16 +0530

    jquery

--- JS/8. jQuery/app.js
+++ JS/8. jQuery/app.js
@@ -1,11 +1,76 @@
-$(document).ready(()=>{
-    console.log("Jquery is ready")
+// let p = document.querySelector('p')
+// p.innerText = "javascript"
+// $('p').text("javascript")
+
+
+// hide 
+
+$('.hide').click(() => {
+    $('.container').hide(1500, () => {
+        console.log("Hide done")
+    })
 })
 
-function change(){
-    $('p').html('this is <i>javascript</i>')
-}
+$('.show').click(() => {
+    $('.container').show(1500, () => {
+        console.log("show done")
+    })
+})
 
-// effect - hide, show, fade in fade out , fade to, fade toggle
-// slideup , slide down , slidetoggle
-// animate
+// fadein , fade out , fade to , fade toggle
+
+$('.fadeOut').click(() => {
+    $('.container').fadeOut(1500, () => {
+        console.log("fade out done")
+    })
+})
+
+
+$('.fadeIn').click(() => {
+    $('.container').fadeIn(2000, () => {
+        console.log("fade In done")
+    })
+})
+
+
+$('.fadeTo').click(() => {
+    $('.container').fadeTo(2000, 0.5, () => {
+        console.log("fade To done")
+    })
+})
+
+
+$('.fadeToggle').click(() => {
+    $('.container').fadeToggle(1000, () => {
+        console.log("fade Toggle done")
+    })
+})
+
+
+$('.slideup').click(() => {
+    $('.container').slideUp(1000, () => {
+        console.log("slide Up done")
+    })
+})
+
+$('.slidedown').click(() => {
+    $('.container').slideDown(1000, () => {
+        console.log("slide Down done")
+    })
+})
+
+$('.slidetoggle').click(() => {
+    $('.container').slideToggle(1000, () => {
+        console.log("slide Toggle done")
+    })
+})
+
+
+$('.animate').click(() => {
+    $('.container').animate({
+        width: "+=50px",
+        height: "+=50px",
+        opacity: '-=0.2'
+        //1 - 0.8, 0.6,0.4,0.2,0
+    })
+})
